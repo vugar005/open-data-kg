8f2fc99724e6061455e6131d8f3a092adf6105d5
vugar <abdullayev1.618@hotmail.com>
2018-10-10 20:51:45 +0400

    angular font-awesome intergrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,22 @@ import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
 import { AuthModule } from './auth/auth.module';
-import { LandingPageModule } from './landing-page/landing-page.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './landing-page/header/header.component';
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     AuthModule,
-    LandingPageModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
