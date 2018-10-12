e3c1c08911116f7c2c728adfd8d841dbf7981276
vugar <abdullayev1.618@hotmail.com>
2018-10-12 10:27:36 +0400

    wave particles config changed

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { AppState } from '../../reducers';
 import { Store } from '@ngrx/store';
 import { SetToken, TryLogin } from '../store/auth.actions';
-
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { fadeIn } from 'ng-animate';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger('fadeIn', [transition(':enter', useAnimation(fadeIn))])
+  ]
 })
 export class LoginComponent implements OnInit {
-
+  @HostBinding('@fadeIn') animate = true;
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
--- src/app/landing-page/main-page/main-page.component.ts
+++ src/app/landing-page/main-page/main-page.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  OnDestroy,
+  HostListener
+} from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 declare var THREE;
@@ -9,12 +18,13 @@ declare var THREE;
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChild('container') elementRef: ElementRef;
+  @ViewChild('container')
+  elementRef: ElementRef;
   faSearch = faSearch;
   faChevronRight = faChevronRight;
-  SEPARATION = 40;
-  AMOUNTX = 35;
-  AMOUNTY = 35;
+  SEPARATION = 100;
+  AMOUNTX = 50; // 35
+  AMOUNTY = 50; // 35
   particles = [];
   particle;
   count = 0;
@@ -27,10 +37,17 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
   scene = THREE.Scene;
   renderer;
   req: any;
+  mouseX = 170;
+  mouseY = -294;
+  @HostListener('mousemove')
+  mousemove() {this.onDocumentMouseMove(event);}
+  @HostListener('touchstart')
+  touchstart() {this.onDocumentTouchStart(event); }
+  @HostListener('touchemove')
+  touchemove() {this.onDocumentTouchMove(event); }
   constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   ngAfterViewInit() {
     this.init();
     this.animate();
@@ -38,30 +55,35 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
   ngOnDestroy() {
     cancelAnimationFrame(this.req);
   }
+  resetMouse() {
+    console.log('on')
+    this.mouseX = 170;
+    this.mouseY = -294;
+  }
 
   init() {
     this.container = this.elementRef.nativeElement;
-   // document.body.appendChild(this.container);
+    // document.body.appendChild(this.container);
     if (this.container) {
       this.container.className += this.container.className ? ' waves' : 'waves';
     }
     this.camera = new THREE.PerspectiveCamera(
-      120, // 120
+      75, // 120
       window.innerWidth / window.innerHeight,
-      0.1,
-      2000
+      1, // 0.1
+      10000 // 2000
     );
-    this.camera.position.y = 100;
-    this.camera.position.z = 50; // 300
-     this.camera.rotation.x = 0.25; // 0.35
-   this.scene = new THREE.Scene();
+    //  this.camera.position.y = 100;
+    this.camera.position.z = 1000; // 50
+    //  this.camera.rotation.x = 0.25; // 0.35
+    this.scene = new THREE.Scene();
     this.particles = new Array();
     const PI2 = Math.PI * 2;
     const material = new THREE.SpriteCanvasMaterial({
       color: 0xffffff,
       program: function(context) {
         context.beginPath();
-        context.arc(0, 0, 0.1, 0, PI2, true);
+        context.arc(0, 0, 0.2, 0, PI2, true); // (0, 0, 0.1, 0, PI2
         context.fill();
       }
     });
@@ -73,16 +95,15 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
         this.particle.position.x =
           ix * this.SEPARATION - (this.AMOUNTX * this.SEPARATION) / 2;
         this.particle.position.z =
-          iy * this.SEPARATION - (this.AMOUNTY * this.SEPARATION - 10);
+          iy * this.SEPARATION - (this.AMOUNTY * this.SEPARATION) / 2;
         this.scene.add(this.particle);
       }
     }
+
     this.renderer = new THREE.CanvasRenderer({alpha: true });
-   // this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-   // this.renderer.setClearColor(0xfffff0, 1);
     this.container.appendChild(this.renderer.domElement);
-   // window.addEventListener('resize', this.onWindowResize, false);
   }
   onWindowResize() {
     this.windowHalfX = window.innerWidth / 2;
@@ -92,12 +113,14 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
   animate() {
-   this.req =  requestAnimationFrame(this.animate.bind(this));
+    this.req = requestAnimationFrame(this.animate.bind(this));
 
-
-      this.render();
+    this.render();
   }
   render() {
+    this.camera.position.x += (this.mouseX - this.camera.position.x) * 0.05;
+    this.camera.position.y += (-this.mouseY - this.camera.position.y) * 0.05;
+    this.camera.lookAt(this.scene.position);
     let i = 0;
     for (let ix = 0; ix < this.AMOUNTX; ix++) {
       for (let iy = 0; iy < this.AMOUNTY; iy++) {
@@ -112,6 +135,29 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
     }
     this.renderer.render(this.scene, this.camera);
     // This increases or decreases speed
-    this.count += 0.05;
+    this.count += 0.1;
+  }
+  onDocumentMouseMove(event) {
+    this.mouseX = event.clientX - this.windowHalfX;
+  //  console.log(event.clientY - this.windowHalfY);
+    if (event.clientY - this.windowHalfY > -100) {return; }
+    this.mouseY = event.clientY - this.windowHalfY;
+  //  console.log(this.mouseY);
+  }
+   onDocumentTouchStart( event ) {
+     console.log('touchstart');
+    if ( event.touches.length === 1 ) {
+      event.preventDefault();
+      this.mouseX = event.touches[ 0 ].pageX - this.windowHalfX;
+      this.mouseY = event.touches[ 0 ].pageY - this.windowHalfY;
+    }
+  }
+ onDocumentTouchMove( event ) {
+  console.log('touchmove');
+    if ( event.touches.length === 1 ) {
+      event.preventDefault();
+      this.mouseX = event.touches[ 0 ].pageX - this.windowHalfX;
+      this.mouseY = event.touches[ 0 ].pageY - this.windowHalfY;
+    }
   }
 }
