4330132bb7bc0cc568de11db4b6751848ea3f1c2
vugar <abdullayev1.618@hotmail.com>
2018-10-30 18:39:13 +0400

    minor fixes

--- src/app/shared/methods/loadExternals.ts
+++ src/app/shared/methods/loadExternals.ts
@@ -1,4 +1,5 @@
 const scripts = [];
+const styles = [];
 export function loadExternalScripts(url: string) {
  const isLoaded = scripts.find(scr => scr === url);
  if (isLoaded) { return Promise.resolve(true); }
@@ -11,3 +12,15 @@ export function loadExternalScripts(url: string) {
     document.head.appendChild(scriptElement);
   });
 }
+export function loadExternalStyles(url: string) {
+  const isLoaded = styles.find(scr => scr === url);
+  if (isLoaded) { return Promise.resolve(true); }
+   return new Promise((resolve, reject) => {
+     const styleElement = document.createElement('link');
+    styleElement.href = url;
+    styleElement.rel = 'stylesheet';
+    styleElement.onload = resolve;
+     styles.push(url);
+     document.head.appendChild(styleElement);
+   });
+ }
